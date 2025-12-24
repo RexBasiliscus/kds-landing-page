9569da582771adaf3ef0e8684cb026392d429aa6
RexBasiliscus <alexander-219@outlook.com>
2025-12-24 15:02:39 +0100

    removed the autoreply emailJS functionality

--- src/utils/emailService.js
+++ src/utils/emailService.js
@@ -2,19 +2,24 @@ import emailjs from "@emailjs/browser";
 
 const SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICE_ID;
 const CONTACT_TEMPLATE_ID = import.meta.env.VITE_EMAILJS_CONTACT_TEMPLATE_ID;
-const AUTOREPLY_TEMPLATE_ID = import.meta.env
-  .VITE_EMAILJS_AUTOREPLY_TEMPLATE_ID;
+// const AUTOREPLY_TEMPLATE_ID = import.meta.env
+//   .VITE_EMAILJS_AUTOREPLY_TEMPLATE_ID;
 const PUBLIC_KEY = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
 
 export const sendContactEmail = async (formElement) => {
   // Send Contact Us email to you
-  await emailjs.sendForm(SERVICE_ID, CONTACT_TEMPLATE_ID, formElement, PUBLIC_KEY);
-  
-  // Send Auto-Reply email to the user
   await emailjs.sendForm(
     SERVICE_ID,
-    AUTOREPLY_TEMPLATE_ID,
+    CONTACT_TEMPLATE_ID,
     formElement,
     PUBLIC_KEY
   );
+
+  // Auto-Reply email to user (disabled for now)
+  // await emailjs.sendForm(
+  //   SERVICE_ID,
+  //   AUTOREPLY_TEMPLATE_ID,
+  //   formElement,
+  //   PUBLIC_KEY
+  // );
 };
